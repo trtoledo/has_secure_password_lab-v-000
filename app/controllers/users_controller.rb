bb949d1cfdfc1bcafd2a4afdd6f2b52f244860c9
Tomas Toledo <trtoledo1977@gmail.com>
2020-04-14 15:17:47 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.name = params[:user][:name]
-    @user.name = params[:user][:password]
+    @user.password = params[:user][:password]
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
