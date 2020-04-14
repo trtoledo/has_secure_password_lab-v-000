2607ec2355772f3c1c9fb85c1fd9d17de5ca889e
Tomas Toledo <trtoledo1977@gmail.com>
2020-04-14 18:42:41 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.password = params[:user][:password]
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render 'users/new'
-    end
+    if params[:user][:password] == params[:user][:password_confirmation]
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+      else
+        redirect_to '/users/new'
+      end
+    # @user = User.new
+    # @user.name = params[:user][:name]
+    # @user.password = params[:user][:password]
+    # if @user.save
+    #   session[:user_id] = @user.id
+    #   redirect_to root_path
+    # else
+    #   render 'users/new'
+    # end
   end
 
 
